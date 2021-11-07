fee4766779812d33634bf30d65f3514f78b92a8f
Amy Posten <amyposten85@gmail.com>
2021-11-07 12:34:18 -0500

    commit tree a191beaf218901f1cc55cb31217dfbb4420b10aa

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,78 @@
-# your code goes here
+class Person
+    attr_reader :name, :happiness, :hygiene
+
+    attr_accessor :bank_account
+    
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(amount)
+        if amount > 10
+            @happiness = 10
+        elsif amount <= 0
+            @happiness = 0
+        else
+            @happiness = amount
+        end
+    end
+
+    def hygiene=(amount)
+        if amount > 10
+            @hygiene = 10
+        elsif amount <= 0
+            @hygiene = 0
+        else
+            @hygiene = amount
+        end
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+    end    
+end
+
+
